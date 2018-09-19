e7d29e5c401bac68394f0d9c3b8e526064a4de52
amisolution <siriusgem1@gmail.com>
2018-09-19 10:29:25 +0200

    Update web3.js

--- src/web3.js
+++ src/web3.js
@@ -1,5 +1,5 @@
 var Web3 = require('web3');
 var web3 = new Web3();
-import Web3 from 'web3';
+// import Web3 from 'web3';
 const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
 web3.eth.getAccounts().then(console.log);
