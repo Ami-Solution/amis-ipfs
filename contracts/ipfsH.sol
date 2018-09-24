e7751b89cb86d8bcfcc419805c70cd8f17b9b9f4
amisolution <siriusgem1@gmail.com>
2018-09-24 11:23:39 +0200

    Create ipfsH.sol

---
+++ contracts/ipfsH.sol
@@ -0,0 +1,15 @@
+//0xdf71BA382Fe1861B4197DE77759349d7ccf2783E  contract address on ropsten
+//deployed using remix 
+
+pragma solidity ^0.4.25;
+contract ipfsH {
+ string ipfsHash;
+ 
+ function sendHash(string x) public {
+   ipfsHash = x;
+ }
+
+ function getHash() public view returns (string x) {
+   return ipfsHash;
+ }
+}
