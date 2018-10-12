bfd94b26cd0e86e1ea57a3f23c51c6142e33f398
Glitch (dull-snail) <none>
2018-10-12 20:12:15 +0000

    🚂🐯 Updated with Glitch

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-/*https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-a-stylesheet*/
+/* import 'bootstrap/dist/css/bootstrap.min.css';
+/* https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-a-stylesheet*/
 /* import ‘bootstrap/dist/css/bootstrap.css’;*/
 /* import ‘bootstrap/dist/css/bootstrap-theme.css’;*/
 ReactDOM.render(
